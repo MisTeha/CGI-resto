d365f9ab64c2dca7c6695068b5529ec6237f8107
MisTeha <uku.mihkel.kolka@gmail.com>
2026-03-10 20:09:28 +0200

    java project init with start.spring.io

---
+++ backend/src/main/java/com/umkolka/cgiresto/CgirestoApplication.java
@@ -0,0 +1,13 @@
+package com.umkolka.cgiresto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgirestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgirestoApplication.class, args);
+	}
+
+}
